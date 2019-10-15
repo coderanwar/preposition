e0392d1ddfbc589de2381428422904d8024b6925
Anwar Hossain <anwar.ubuntu@gmail.com>
2019-10-15 17:13:10 +0600

    initial commit

---
+++ pp/js/index.js
@@ -0,0 +1,4 @@
+/*var website = 'http://w3cp.blogspot.com';
+var open = '_system';
+var footLink = '<h4><a href="#" onclick="window.open('+website+', '+open+');">w3cp.blogspot.com</a></h4>';
+document.getElementById(footerLink).innerHTML= footLink;*/
